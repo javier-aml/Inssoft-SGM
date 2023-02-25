d5c4569f231aa127192aaeb70263968a1a251d5b
Hugo Figueroa <hugo.figueroa@inssoftmx.com>
2023-02-24 20:22:01 -0600

    Change balance POST

--- src/controllers/balance.controller.js
+++ src/controllers/balance.controller.js
@@ -34,10 +34,10 @@ class BalanceController {
 
 
 
-        //const salesDicSecondPart = await balanceHelper.getInvoicesNatgasSecondPart(rfc,'2022-12-01','2023-01-01','V');// Trae el resto
+        const salesDicSecondPart = await balanceHelper.getInvoicesNatgasSecondPart(rfc,'2022-12-01','2023-01-01','V');// Trae el resto
         //const salesDicThirdPart = await balanceHelper.getInvoicesNatgasThirdPart(rfc,'2022-12-01','2023-01-01','V');// Trae el resto
         
-        const allSales = [...salesData];
+        const allSales = [...salesData,...salesDicSecondPart];
         console.log(allSales.length)
         const salesResult = balanceHelper.validationsNatgas(allSales);
 
